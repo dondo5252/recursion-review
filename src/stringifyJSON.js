6f88ef75b35ed54c73882c25b0ddf42622085e21
Jonathan Oh <jonoh832@gmail.com>
2022-05-31 12:31:03 -0700

    Got to object {"a" : "apple"}

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // if typeof is boolean OR if typeof is num OR if typeof is string
+  var type = typeof obj;
+  if (type === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (type === 'number' || type === 'boolean') {
+    return obj.toString();
+  }
+
+  if (Array.isArray(obj)) {
+    var tempArray = [];
+    for (var i = 0; i < obj.length; i++) {
+      tempArray.push(stringifyJSON(obj[i]));
+    }
+    console.log('before: ', tempArray);
+    tempArray.join(', ');
+
+    console.log('after: ', tempArray);
+    return '[' + tempArray + ']';
+  }
+
+  if (!obj) {
+    return 'null';
+  }
+
+  if (type === 'object') {
+
+    if (obj) {
+      return '{}';
+    }
+    for (var key in obj) {
+      return '{' + stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + '}';
+    }
+  }
+
+  // if function
+
 };
